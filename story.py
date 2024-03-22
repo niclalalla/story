d3e8e8800675346278d925b327168ff24435af8d
Nic Chen <niclalalla@163.com>
2024-03-22 16:18:49 +0800

    初始化

---
+++ story.py
@@ -0,0 +1,57 @@
+import urllib.request
+from lxml import etree
+
+def create_request(url):
+    '''
+        构造请求request
+    '''
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+    }
+    request = urllib.request.Request(url = url, headers = headers)
+    return request
+
+def get_content(request):
+    '''
+        得到响应内容
+    '''
+    response = urllib.request.urlopen(request)
+    content = response.read().decode('utf-8')
+    return content
+
+
+if __name__ == '__main__':
+    # 获取所有章节
+    # 史上最强练气期
+    # base_url = 'http://www.yetianlian.cc/yt4017/'
+    # 夜的命名术
+    base_url = 'http://www.yetianlian.cc/yt20281/'
+    request = create_request(base_url)
+    content = get_content(request)
+    base_tree = etree.HTML(content)
+    # 章节名
+    name_list = base_tree.xpath('//div[@class="listmain"]/dl/dd/a/text()')
+    # 章节地址
+    url_list = base_tree.xpath('//div[@class="listmain"]/dl/dd/a/@href')
+
+    # 定位到从哪一章开始读
+    key = input('请输入要阅读的章节：')
+    begin = 0
+    for i in range(0, len(name_list)-1):
+        if(key in name_list[i]):
+            begin = i
+
+
+    for i in range(begin, len(name_list)-1):
+        input('章节名---------------------->' + name_list[i])
+        # 获取具体哪一章的内容
+        url = 'http://www.yetianlian.cc' + url_list[i]
+        request = create_request(url)
+        content = get_content(request)
+        tree = etree.HTML(content)
+        # 获取小说的内容
+        result = tree.xpath('//div[@id="content"]/text()')
+        # 遍历内容
+        for res in result:
+            input(res)
+    print('-------------->end')
